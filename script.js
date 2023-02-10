8c6ba90a4ff3e95eb06ebbf284c29751f9243778
Kari Danchelle <123973263+DanchellexDanchelle@users.noreply.github.com>
2023-02-10 03:11:40 -0600

    lab
    css understood not js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-let name = 'Danchelle';
-console.log(name);
+
+let 'name' = ''
+let "hacker" = "robot"
+function whosthere(){
+    if("hacker") { 
+        greeting = "not today satan";
+        else('name'){
+            greeting = "welcome fellow Modder";
+        }
+    }
+}
